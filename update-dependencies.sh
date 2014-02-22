3deadac4ed5d273bc5e1b78360b11b27604b233b
Octavio Amuchástegui <octavioamuchastegui@gmail.com>
2014-02-22 01:58:17 -0300

    Create update-dependencies.sh

---
+++ update-dependencies.sh
@@ -0,0 +1,3 @@
+path=`dirname $0`
+cd $path
+npm install  grunt grunt-csso grunt-spritesmith grunt-html-build grunt-contrib-jshint grunt-contrib-uglify grunt-contrib-htmlmin grunt-contrib-watch grunt-concurrent grunt-notify  grunt-contrib-imagemin grunt-contrib-connect grunt-open grunt-contrib-concat grunt-contrib-copy grunt-bower-task grunt-contrib-csslint load-grunt-tasks grunt-recess --save-dev
